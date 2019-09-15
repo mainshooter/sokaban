e163cf79aaf0f68cfb7b23c751ec3979a9b7c2a0
woutvdpol <woutvdpol@gmail.com>
2019-09-15 19:54:30 +0200

    Startview (#5)

--- DKD-Sokaban/controller/Controller.cs
+++ DKD-Sokaban/controller/Controller.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using DKD_Sokaban.view;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DKD_Sokaban {
-    class Controller {
+
+  public class Controller {
         private Game Game;
 
         public Controller () {
@@ -13,7 +15,32 @@ namespace DKD_Sokaban {
         }
 
         public void Start() {
+            StartView start = new StartView(this);
+        }
 
+        public void StartViewInput(string input)
+        {
+            switch (input)
+            {
+                case "1":
+                    throw new NotImplementedException();
+                    break;
+                case "2":
+                    throw new NotImplementedException();
+                    break;
+                case "3":
+                    throw new NotImplementedException();
+                    break;
+                case "4":
+                    throw new NotImplementedException();
+                    break;
+                case "s":
+                    throw new NotImplementedException();
+                    break;
+                default:
+                    throw new NotImplementedException();
+                    break;
+            }
         }
 
         public void LoadGame(int index) {
---
+++ DKD-Sokaban/view/StartView.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DKD_Sokaban.view
+{
+    public class StartView
+    {
+        Controller localController;
+        public StartView(Controller controller)
+        {
+            localController = controller;
+            ShowStartView();
+        }
+
+        public void ShowStartView()
+        {
+            Console.WriteLine("+-------------------------------------+");
+            Console.WriteLine("|  Welkom bij Sokoban                 |");
+            Console.WriteLine("|                                     |");
+            Console.WriteLine("|  Gebruiksaanwijzingen               |");
+            Console.WriteLine("|                                     |");
+            Console.WriteLine("|  M : muur                           |");
+            Console.WriteLine("|  . : vloer                          |");
+            Console.WriteLine("|  o : krat                           |");
+            Console.WriteLine("|  0 : krat op bestemming             |");
+            Console.WriteLine("|  x : bestemming                     |");
+            Console.WriteLine("|  @ : speler                         |");
+            Console.WriteLine("+-------------------------------------+");
+
+            Console.Write("Kies een doolhof (1-4), s = stop: ");
+
+            localController.StartViewInput(Console.ReadLine());
+        }
+
+
+    }
+}
