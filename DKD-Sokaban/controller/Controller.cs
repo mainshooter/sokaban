ab1628faa094d29e1443f01ed51371e7e5e0f702
Wout <woutvdpol@gmail.com>
2019-09-15 19:43:40 +0200

    Startview

--- DKD-Sokaban/controller/Controller.cs
+++ DKD-Sokaban/controller/Controller.cs
@@ -1,18 +1,44 @@
-﻿using System;
+﻿using DKD_Sokaban.view;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DKD_Sokaban {
-    class Controller {
+    public class Controller {
 
         public Controller () {
 
         }
 
         public void Start() {
+            StartView start = new StartView(this);
+        }
 
+        public void StartViewInput(string input)
+        {
+            switch (input)
+            {
+                case "1":
+                    throw new NotImplementedException();
+                    break;
+                case "2":
+                    throw new NotImplementedException();
+                    break;
+                case "3":
+                    throw new NotImplementedException();
+                    break;
+                case "4":
+                    throw new NotImplementedException();
+                    break;
+                case "s":
+                    throw new NotImplementedException();
+                    break;
+                default:
+                    throw new NotImplementedException();
+                    break;
+            }
         }
     }
 }
